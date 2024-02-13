6c7d0af278f5d56ff04c2fd71c76b9cc3b347100
Sofiya <sofik_shu2@mail.ru>
2024-02-13 23:06:16 +0300

    tasks 3 and 4 done

---
+++ 2/lab2/3.py
@@ -0,0 +1,5 @@
+num = int(input('Enter a number: '))
+n = 0
+while n < 11:
+    print(num * n, end=' ')
+    n = n + 1
---
+++ 2/lab2/4.py
@@ -0,0 +1,33 @@
+# for i in range(1,16):
+    # print(i, end=", ")
+
+# for i in [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]:
+    # print(i+1, end=' ')
+
+# n = 21
+# for i in range(19):
+    # for j in range(1, 2):
+        # n = n - j
+        # print(n, end=' ')
+
+# elems = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+# for index, elem in enumerate(elems):
+    # if elem % 2 == 0:
+        # print(elem, end=" ")
+
+# for i in range(4, 45, 4):
+    # print(i, end=" ")
+
+# for i in range(1, 29):
+    # if i % 5 == 0:
+        # print(i, end = " ")
+    # else:
+        # continue
+
+# names = ["Sofiya", "Mary", "David", "Bob", "Jack"]
+# ages = [20, 30, 40, 50, 60]
+# for name, age in zip(names, ages):
+    # print(f"{name} is {age} years")
+
+for i in range(80, -1, -10):
+    print(i, end = ' ')
