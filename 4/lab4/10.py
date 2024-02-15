e85a1859645fc135cafe10b140213913bda76385
Sofiya <sofik_shu2@mail.ru>
2024-02-15 14:46:32 +0300

    Tasks 9, 10 from lab 4 done

---
+++ 4/lab4/10.py
@@ -0,0 +1,4 @@
+names = ['Sofia', 'Samuel', 'Joseph', 'Ann', "Mary", "Sarah"]
+new_names = list(filter(lambda name: name[0] == 'S', names))
+print(new_names)
+
---
+++ 4/lab4/9.py
@@ -0,0 +1,3 @@
+summary = lambda *args: sorted(args)
+print(summary(1, 3, 2))
+print(summary(10, 25, -5))
