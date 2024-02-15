44284e546276141eed3d1a911e377b60f3a6c6b4
Sofiya <sofik_shu2@mail.ru>
2024-02-15 13:11:46 +0300

    Task 2 from lab 4 done

---
+++ 4/lab4/2.py
@@ -0,0 +1,7 @@
+def merge_strings(str1, str2):
+    return str1 + " " + str2
+
+
+print(merge_strings("Hello", "World!"))
+print(merge_strings("This is my first string", "This is my second string"))
+print(merge_strings("1 + 2", "= 3"))
