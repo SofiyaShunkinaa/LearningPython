7ac34db113966fc1ca936d053ee52924485b8283
Sofiya <sofik_shu2@mail.ru>
2024-02-16 22:06:34 +0300

    points 1.3.9, 1.3.10 done in lab 9

--- 9/lab9/1.py
+++ 9/lab9/1.py
@@ -49,13 +49,31 @@ enabled = IntVar()
 check = ttk.Checkbutton(text="Turn on", variable=enabled, command=checkbutton_changed())
 check.pack(anchor=N, padx=5, pady=5)
 
-langs = ['Python', 'Java', 'C']
+langs = ['Python', 'Java', 'C', 'C++', 'Javascript', 'HTML', 'TypeScript', 'C#', 'PHP']
 lang_vars = Variable(value=langs)
 lang_list = Listbox(listvariable=lang_vars, height=5)
 lang_list.pack(side=RIGHT, padx=5)
 
 combo = ttk.Combobox(values=langs)
 combo.pack(fill=X)
+
+menu = Menu()
+options = Menu(tearoff=0)
+addional = Menu(tearoff=0)
+addional.add_command(label="Commit and push")
+addional.add_command(label="Undo")
+addional.add_command(label="Redo")
+options.add_cascade(label="Commit", menu=addional)
+options.add_command(label="Push")
+menu.add_cascade(label="Options", menu=options)
+root.config(menu=menu)
+
+lang_str = StringVar(value=langs)
+listbox = Listbox(listvariable=lang_str, height=5)
+listbox.pack(side=LEFT)
+scroll = ttk.Scrollbar(orient='vertical', command=listbox.yview)
+scroll.pack(side=RIGHT, fill=Y)
+
 
 root.protocol("WM_DELETE_WINDOW", finish)
 root.mainloop()
