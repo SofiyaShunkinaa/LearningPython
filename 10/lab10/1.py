cd16074f6317101dc5a8243676561ad465f4f451
Sofiya <sofik_shu2@mail.ru>
2024-02-17 22:43:59 +0300

    lab 10 done

--- 10/lab10/1.py
+++ 10/lab10/1.py
@@ -2,6 +2,7 @@ import random
 from tkinter import *
 from tkinter import ttk
 import time
+from tkinter import messagebox
 
 # Window sizes
 WIN_H = 600
@@ -18,12 +19,22 @@ def move_snake(event):
         canvas.move(snake, 0, -10)
     if event.keysym == 'Down':
         canvas.move(snake, 0, 10)
+    check_position()
 
 
 def check_position():
     snake_cords = canvas.coords(snake)
+    global score
     for config in bugs:
-        bug_cords = canvas.coords(bug)
+        bug_cords = canvas.coords(config['bug'])  # Получаем координаты текущего объекта bug
+        if bug_cords[0] - 50 < snake_cords[0] and bug_cords[0] + 50 > snake_cords[0] and bug_cords[1] - 50 < snake_cords[1] and bug_cords[1] + 50 > snake_cords[1]:
+            canvas.delete(config['bug'])
+            bugs.remove(config)
+            score += 1
+            score_label['text'] = "Score: " + str(score)
+    if score == 7:
+        messagebox.showinfo(title="Victory", message="You win!")
+        close()
 
 
 def close():
@@ -31,30 +42,56 @@ def close():
     running = False
 
 
+def start():
+    for config in bugs:
+        config['speed_x'] = random.randint(0, 2)-1
+        config['speed_y'] = random.randint(0, 2)-1
+    canvas['bg'] = 'wheat2'
+
+
+def stop():
+    for config in bugs:
+        config['speed_x'] = 0
+        config['speed_y'] = 0
+    canvas['bg'] = 'red'
+
+
 # Window creation
 window = Tk()
 window.title("Snake Game")
 window.protocol("WM_DELETE_WINDOW", close)
+window.geometry("%dx%d+%d+%d" % (WIN_W, WIN_H+50, 450, 50))
 
 # Game images
 snake_img = PhotoImage(file="snake.png").subsample(8, 8)
 bug_img = PhotoImage(file="bug.png").subsample(10, 10)
 
 # Layout creation
+canvas1 = Canvas(window, height=50, bg='wheat1')
+canvas1.pack(fill=X)
 canvas = Canvas(window, width=WIN_W, height=WIN_H, bg='wheat2')
 canvas.pack()
+score_label = ttk.Label(canvas1, font=('Helvetica'), text="Score: 0")
+score_label.pack(side=LEFT, padx=5, pady=5)
+btn_pause = ttk.Button(canvas1, text="Pause", command=stop)
+btn_resume = ttk.Button(canvas1, text="Resume", command=start)
+btn_resume.pack(side=RIGHT, padx=5, pady=5)
+btn_pause.pack(side=RIGHT, padx=5, pady=5)
+
+
 
 # Objects creation
 bugs = []
 snake = canvas.create_image(40, 40, image=snake_img)
 for i in range(7):
-    bug = canvas.create_image(random.randint(50, 650), random.randint(50, 550), image=bug_img)
-    speed_x = random.randint(-1, 1)
-    speed_y = random.randint(-1, 1)
+    bug = canvas.create_image(random.randint(50, 450), random.randint(50, 550), image=bug_img)
+    speed_x = random.randint(0, 2)-1
+    speed_y = random.randint(0, 2)-1
     config = {'bug': bug, 'speed_x': speed_x, 'speed_y': speed_y}
     bugs.append(config)
+score = 0
 
-canvas.bind_all("<Key>", move_snake)
+
 
 # Game cycle
 running = True
@@ -64,10 +101,13 @@ while running:
         canvas.move(config['bug'], config['speed_x'], config['speed_y'])
 
         coords = canvas.coords(config['bug'])
-        if coords[0] < 0 or coords[0] > WIN_W:
+        if coords[0]-20 < 0 or coords[0]+20 > WIN_W:
             config['speed_x'] = -config['speed_x']
-        if coords[1] < 0 or coords[1] > WIN_H:
+        if coords[1]-20 < 0 or coords[1]+20 > WIN_H:
             config['speed_y'] = -config['speed_y']
+
+    canvas.bind_all("<Key>", move_snake)
+
 
     # Window update
     window.update()
