16214b0652380994f8b44846fbe1288ef0cb0248
Sofiya <sofik_shu2@mail.ru>
2024-03-02 23:56:38 +0300

    add: routing to different pages

--- firstDjangoProject/pythonProject/first_project/first_project/urls.py
+++ firstDjangoProject/pythonProject/first_project/first_project/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from hello import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index),
+    path('about', views.about),
+    path('contact', views.contact),
 ]
 
