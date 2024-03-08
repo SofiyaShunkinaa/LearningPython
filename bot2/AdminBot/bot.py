2ec24d8bb7a6267bcb92af7f9314d57b094643f3
Sofiya <sofik_shu2@mail.ru>
2024-03-08 22:54:44 +0300

    add: handler for booking, connection to db

--- bot2/AdminBot/bot.py
+++ bot2/AdminBot/bot.py
@@ -6,12 +6,23 @@ import datetime
 TOKEN = '6815877539:AAGbl4UAI-JTsTDEjxLR88WHuab76rqSEeI'
 bot = TeleBot(TOKEN)
 
+conn = sqlite3.connect('fidel.db')
+cur = conn.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS bookings (id INTEGER PRIMARY KEY, name VARCHAR(50), phone VARCHAR(50), ')
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'Hello')
+    bot.send_message(message.chat.id, 'Привет!')
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, 'Help')
+    bot.send_message(message.chat.id, '''
+    Чтобы записаться выбери 
+    ''')
+
+
+@bot.message_handler(commands=['Записаться'])
+def send_welcome(message):
+
 
 bot.polling(none_stop=True, interval=0)
