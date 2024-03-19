61f4e6e81a2715fa6a0ad68beecb1d222a251c9a
Sofiya <sofik_shu2@mail.ru>
2024-03-19 11:45:43 +0300

    add: traffic cars

--- pygame/pythonProject/main.py
+++ pygame/pythonProject/main.py
@@ -3,6 +3,7 @@ import pygame
 import pygame.freetype
 from my_car import MyCar
 from road import Road
+from traffic import TrafficCar
 
 
 def get_car_img(filename, size, angle):
@@ -20,31 +21,53 @@ screen = pygame.display.set_mode((500, 750))
 pygame.display.set_caption("Симулятор суетолога")
 background_color = (0, 0, 0)
 
-my_car_sound = pygame.mixer.Sound('img/sound.wav')
+my_car_sound = pygame.mixer.Sound('img/sound1.wav')
 my_car_sound.play(-1)
+
+crash_sound = pygame.mixer.Sound('img/crash.wav')
+
 
 road_group = pygame.sprite.Group()
 spawn_road_time = pygame.USEREVENT
 pygame.time.set_timer(spawn_road_time, 1000)
 
+traffic_cars_group = pygame.sprite.Group()
+spawn_traffic_time = pygame.USEREVENT + 1
+pygame.time.set_timer(spawn_traffic_time, 1000)
+
 my_car_img = get_car_img("img/car.png", (130, 80), 90)
 road_img = pygame.image.load("img/road.png")
 road_img = pygame.transform.scale(road_img, (500, 750))
 
+traffic_car_images = []
+traffic_car1 = get_car_img("img/car1.png", (130, 80), -90)
+traffic_car2 = get_car_img("img/car2.png", (130, 80), -90)
+traffic_car3 = get_car_img("img/car3.png", (200, 80), -90)
+traffic_car_images.extend((traffic_car1, traffic_car2, traffic_car3))
+
 road = Road(road_img, (250, 400))
 road_group.add(road)
-road = Road(road_img, (250, -50))
+road = Road(road_img, (250, -120))
 road_group.add(road)
 
 
 def spawn_road():
-    road = Road(road_img, (250, -600))
-    road_group.add(road)
+    road_bg = Road(road_img, (250, -600))
+    road_group.add(road_bg)
+
+
+def spawn_traffic():
+    position = (random.choice([60, 190]), random.randint(-60, -40))
+    speed = random.randint(7, 10)
+    traffic_car = TrafficCar(random.choice(traffic_car_images), position, speed)
+    traffic_cars_group.add(traffic_car)
 
 
 def draw_all():
     road_group.update()
     road_group.draw(screen)
+    traffic_cars_group.update()
+    traffic_cars_group.draw(screen)
     my_car.draw(screen)
 
 
@@ -57,10 +80,13 @@ while running:
             running = False
         if event.type == spawn_road_time:
             spawn_road()
+        if event.type == spawn_traffic_time:
+            spawn_traffic()
 
     screen.fill(background_color)
     my_car.move()
     draw_all()
+    my_car.crash(crash_sound, traffic_cars_group)
 
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(90)
