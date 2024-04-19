8c7fe8cd8f6628495df7e08da56d130e03a23af0
Sofiya <sofik_shu2@mail.ru>
2024-04-19 23:55:21 +0300

    added errors class

--- task3/pythonProject/Errors.py
+++ task3/pythonProject/Errors.py
@@ -1,14 +1,15 @@
-def err1():
-    print("""Unexpected count of arguments!
-             Tip: Enter odd number of arguments separated by space""")
+class Errors:
+    @staticmethod
+    def err1():
+        print("""Unexpected count of arguments!
+                 Tip: Enter odd number of arguments separated by space""")
 
+    @staticmethod
+    def err2():
+        print("""Syntax error!
+                 Tip: Enter unique arguments""")
 
-def err2():
-    print("""Syntax error!
-             Tip: Enter unique arguments""")
-
-
-def err3():
-    print("""Too little of arguments!
-             Tip: Enter odd number of arguments (more than 3) separated by space""")
-
+    @staticmethod
+    def err3():
+        print("""Too little of arguments!
+                 Tip: Enter odd number of arguments (more than 3) separated by space""")
