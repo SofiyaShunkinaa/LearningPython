c5ead477f954823e906bfb9687c206e54ef7f432
Sofiya <sofik_shu2@mail.ru>
2024-04-19 23:55:58 +0300

    changed main file structure

--- task3/pythonProject/main.py
+++ task3/pythonProject/main.py
@@ -1,30 +1,10 @@
 import sys
-import Errors
+from Controller import Controller
 
 
-def check_identical(lst):
-    for i in range(len(lst)):
-        if lst.count(lst[i]) > 1:
-            return False
-    return True
-
-
-def input_check(params):
-    print(params)
-    if len(params) % 2 == 0:
-        Errors.err1()
-        new_params = input("Введите параметры через пробел: ").split()
-        input_check(new_params)
-    elif len(params) < 3:
-        Errors.err3()
-        new_params = input("Введите параметры через пробел: ").split()
-        input_check(new_params)
-    elif check_identical(params) == 0:
-        Errors.err2()
-        new_params = input("Введите параметры через пробел: ").split()
-        input_check(new_params)
-    else:
-        print("Принятые параметры:", params)
-
-
-input_check(sys.argv[1:])
+controller = Controller(sys.argv[1:])
+turn = 1
+controller.pc_user_init()
+while turn:
+    turn = int(Controller.get_input(sys.argv[1:]))
+    controller.select_choice(turn)
