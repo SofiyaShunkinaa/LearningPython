0d0a85a23ade05f8d1e6d1a81f690935bf8763d9
Sofiya <sofik_shu2@mail.ru>
2024-04-19 23:56:54 +0300

    added table class

---
+++ task3/pythonProject/Table.py
@@ -0,0 +1,48 @@
+import tabulate
+from testing import get_win_index
+from colorama import Fore, Style
+
+
+class Table:
+    def __init__(self, headers):
+        self.headers = headers
+
+    def generate_table(self):
+        headers_row = self.generate_headers_row()
+        table_data = [headers_row] + self.generate_rows()
+        results = self.format_table(table_data)
+        print(results)
+
+    def generate_headers_row(self):
+        return ['v PC\\User >'] + [f'{move}' for move in self.headers]
+
+    def generate_rows(self):
+        rows = []
+        for move_col in self.headers:
+            row = self.generate_row(move_col)
+            rows.append(row)
+        return rows
+
+    def generate_row(self, move_col):
+        row = [f'{move_col}']
+        for move_row in self.headers:
+            result = get_win_index(self.headers, move_col, move_row)
+            cell_value = self.format_values(result)
+            row.append(cell_value)
+        return row
+
+    def format_values(self, result):
+        if result == 0:
+            return Fore.LIGHTCYAN_EX + Style.BRIGHT + 'Draw' + Style.RESET_ALL
+        elif result == 1:
+            return Fore.LIGHTRED_EX + Style.BRIGHT + 'Lose' + Style.RESET_ALL
+        elif result == -1:
+            return Fore.LIGHTGREEN_EX + Style.BRIGHT + 'Win' + Style.RESET_ALL
+
+    def format_table(self, table_data):
+        return tabulate.tabulate(table_data, headers='firstrow', tablefmt='grid')
+
+
+# player_moves = ['Rock', 'Paper', '3rd move', '4th', '5th', '6th', '7th']
+# table = Table(player_moves)
+# table.generate_table()
