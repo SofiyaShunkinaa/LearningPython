0719198cab56e068d675405fca5e8c6df5493000
Sofiya <sofik_shu2@mail.ru>
2024-04-20 18:54:39 +0300

    fixed checking

--- task3/pythonProject/main.py
+++ task3/pythonProject/main.py
@@ -6,11 +6,12 @@ def game_cycle(moves):
     controller = Controller(moves)
     controller.pc_user_moves()
     print(f'HMAC: {controller.get_hmac()}')
-    turn = Controller.get_input(moves)
+    turn = Controller.get_menu(moves)
     controller.select_choice(turn)
     return turn
 
 
 running = 1
+checked = Controller.checking(sys.argv[1:])
 while running != "0":
-    running = game_cycle(sys.argv[1:])
+    running = game_cycle(checked)
