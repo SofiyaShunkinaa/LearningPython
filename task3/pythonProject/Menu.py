8851f16d994587e068b92e4aa1372a63839841b8
Sofiya <sofik_shu2@mail.ru>
2024-04-20 17:45:36 +0300

    fixed menu items

--- task3/pythonProject/Menu.py
+++ task3/pythonProject/Menu.py
@@ -6,14 +6,12 @@ class Menu:
     def create_dictionary(moves):
         dictionary = {}
         for i, item in enumerate(moves, 1):
-            dictionary[i] = item
-        dictionary[len(moves) + 1] = 'help'
-        dictionary[0] = 'exit'
+            dictionary[str(i)] = item
+        dictionary['?'] = 'help'
+        dictionary['0'] = 'exit'
         return dictionary
 
     def print_menu(self):
         print("Welcome to the menu, choose one of the following:")
-        menu = ''
         for key, value in self.moves.items():
             print(f"{key} - {value}")
-        print(menu)
