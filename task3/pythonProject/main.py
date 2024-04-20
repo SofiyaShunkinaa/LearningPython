c6bdc00928d5be6a0f1dcaaa8b3e9579f2cdc3df
Sofiya <sofik_shu2@mail.ru>
2024-04-20 17:45:11 +0300

    changed main cycle

--- task3/pythonProject/main.py
+++ task3/pythonProject/main.py
@@ -2,9 +2,15 @@ import sys
 from Controller import Controller
 
 
-controller = Controller(sys.argv[1:])
-turn = 1
-controller.pc_user_init()
-while turn:
-    turn = int(Controller.get_input(sys.argv[1:]))
+def game_cycle(moves):
+    controller = Controller(moves)
+    controller.pc_user_moves()
+    print(f'HMAC: {controller.get_hmac()}')
+    turn = Controller.get_input(moves)
     controller.select_choice(turn)
+    return turn
+
+
+running = 1
+while running != "0":
+    running = game_cycle(sys.argv[1:])
