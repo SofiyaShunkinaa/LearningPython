9d11b13a26909f93e7789c0bfe0386dd4f798aa3
Sofiya <sofik_shu2@mail.ru>
2024-04-20 17:46:12 +0300

    changed class structure

--- task3/pythonProject/PC_player.py
+++ task3/pythonProject/PC_player.py
@@ -1,25 +1,18 @@
-from random import random
+import random
 from SKey import SKey
-from HMAC import Hmac
 
 
-class PC_player:
+class PC:
     def __init__(self, paramCount):
         self.key = None
-        self.hmac = None
+        self.move = None
         self.paramCount = paramCount
 
     def make_choice(self):
-        return random.randint(self.paramCount)
+        self.move = str(random.randint(1, self.paramCount))
+        self.get_key()
 
     def get_key(self):
-        self.key = SKey(32)
-        self.key.generate_secrete_key()
+        key = SKey(32)
+        self.key = key.generate_secrete_key()
         return self.key
-
-    def get_hmac(self, choice_message):
-        self.hmac = Hmac(self.key, choice_message)
-        self.hmac.generate_hmac()
-
-
-
