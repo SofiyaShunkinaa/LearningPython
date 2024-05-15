7a0f7bead25c5c76b3188dbd29fce3695b661f12
Sofiya <sofik_shu2@mail.ru>
2024-05-15 23:33:45 +0300

    added addition to index view

--- kursovoy/project/fidel/app/views.py
+++ kursovoy/project/fidel/app/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
-from app.models import Master
+from app.models import Master, Appointment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import AppointmentForm
 
 
 def index(request):
     masters = Master.objects.all()[:4]
-    return render(request, "index.html", {'masters': masters})
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment = Appointment(
+                first_name=form.cleaned_data['first_name'],
+                last_name=form.cleaned_data['last_name'],
+                service=form.cleaned_data['service'],
+                master=form.cleaned_data['master'],
+                date=form.cleaned_data['date'],
+                phone_number=form.cleaned_data['phone_number']
+            )
+            appointment.save()
+            print("Новая запись создана:")
+            print("Имя:", appointment.first_name)
+            print("Фамилия:", appointment.last_name)
+            print("Дата:", appointment.date)
+            print("Услуга:", appointment.service)
+            print("Мастер:", appointment.master)
+            print("Номер телефона:", appointment.phone_number)
+            return render(request, 'index.html', {'masters': masters,'form': form, 'success': True})
+    else:
+        form = AppointmentForm()
+    return render(request, "index.html", {'masters': masters, 'form': form})
 
 def masters(request):
     masters = Master.objects.all()
@@ -31,3 +54,4 @@ def contacts(request):
 
 def error(request):
     return render(request, "error.html")
+
