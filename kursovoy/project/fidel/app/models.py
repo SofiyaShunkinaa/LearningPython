67f3f0a0433d0585946ce0f28fd36f8311a51974
Sofiya <sofik_shu2@mail.ru>
2024-05-15 21:18:03 +0300

    added master functions

--- kursovoy/project/fidel/app/models.py
+++ kursovoy/project/fidel/app/models.py
@@ -4,6 +4,29 @@ class Master(models.Model):
     name = models.CharField(max_length=100)
     image_path = models.CharField(max_length=200) 
     services = models.TextField() 
+    experience_years = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
+    
+    def get_services_list(self):
+        return self.services.split('\n')
+    
+    def get_experience_display(self):
+        years = self.experience_years
+        if 11 <= years % 100 <= 19:
+            return f"{years} лет"
+        elif years % 10 == 1:
+            return f"{years} год"
+        elif 2 <= years % 10 <= 4:
+            return f"{years} года"
+        else:
+            return f"{years} лет"
+        
+    def get_name_with_ending(self):
+        if self.name.endswith('а') or self.name.endswith('я'):
+            return f"{self.name[:-1]}е"
+        elif self.name.endswith('ий'):
+            return f"{self.name[:-2]}ию"
+        else:
+            return f"{self.name}у"    
