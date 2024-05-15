57d849c2bec0cdf33c1bdc950e6719709ff528c9
Sofiya <sofik_shu2@mail.ru>
2024-05-15 21:19:41 +0300

    adedd pagination and error view

--- kursovoy/project/fidel/app/views.py
+++ kursovoy/project/fidel/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Master
+from app.models import Master
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
@@ -7,7 +8,26 @@ def index(request):
     return render(request, "index.html", {'masters': masters})
 
 def masters(request):
-    return render(request, "masters.html")
+    masters = Master.objects.all()
+    sort_order = request.GET.get('sort', 'asc')
+    if sort_order == 'desc':
+        masters = Master.objects.all().order_by('-experience_years')
+    else:
+        masters = Master.objects.all().order_by('experience_years')
+
+    paginator = Paginator(masters, 4)
+    page = request.GET.get('page')
+    
+    try:
+        masters = paginator.page(page)
+    except PageNotAnInteger:
+        masters = paginator.page(1)
+    except EmptyPage:
+        masters = paginator.page(paginator.num_pages)    
+    return render(request, "masters.html", {'masters': masters, 'sort_order': sort_order})
 
 def contacts(request):
     return render(request, "contacts.html")
+
+def error(request):
+    return render(request, "error.html")
